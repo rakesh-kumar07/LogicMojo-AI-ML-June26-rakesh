86577931f2d39c33039f2beda9d3cd6781799767
rakesh-kumar07 <sonu07jan@gmail.com>
2026-06-27 23:54:57 +0530

    practice

---
+++ week2/class1.py
@@ -0,0 +1,18 @@
+# Exercise 1:
+# Given the list my_list = [10, 20, 30, 40, 50, 60, 70], write a slicing operation to extract the elements from index 2 to 5.
+# Expected Output: [30, 40, 50]
+
+my_list = [10, 20, 30, 40, 50, 60, 70]
+print(my_list[2:5])
+
+
+# Exercise 2:
+# How would you reverse the list my_list using slicing?
+# Expected Code: my_list[::-1]
+print(my_list[::-1])
+
+# Exercise 3:
+# What will be the output of my_list[1:6:3] for the list my_list = [1, 2, 3, 4, 5, 6, 7, 8]?
+# Expected Output: [2, 5]
+my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+print(my_list[1:6:3])
